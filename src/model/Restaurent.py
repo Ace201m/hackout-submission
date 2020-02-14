df4cf0354d71b132fb9c891e5f00bd4549de06da
Khush Chopra <khush.chopra.cse16@itbhu.ac.in>
2020-02-14 18:38:39 +0530

    constructor for restaurant

--- src/model/Restaurent.py
+++ src/model/Restaurent.py
@@ -1,29 +1,12 @@
 from bs4 import BeautifulSoup
 import requests, json
+from Food import Food
 
 link = "https://www.swiggy.com/restaurants/al-baik-world-ravindrapuri-lanka-varanasi-78246"
 link2 = "https://www.swiggy.com/restaurants/capsicum-lanka-varanasi-88160"
 
-res = requests.get(link2)
-
-soup = BeautifulSoup(res.text, 'html.parser')
-# # print(soup.prettify())
-# f = open("res.html",'w+')
-# f.write(soup.prettify())
-# f.close()
-
-res = soup.find_all('script')
 # str(res[3])[35:-9]
-dic = json.loads(str(res[3])[35:-9])
-print(dic)
 # print(str(res[4])[104:-10])
-
-
-def getRestaurant(link):
-    
-
-
-
 
 
 class Restaurent:
@@ -32,21 +15,28 @@ class Restaurent:
 
         res = requests.get(link)
         res = BeautifulSoup(res.text, 'html.parser')
-        res = soup.find_all('script')
+        res = res.find_all('script')
         tempDict = json.loads(str(res[3])[35:-9])
 
 
         self.name = tempDict['name']
         self.city = tempDict['address']['addressRegion']
-        self.image = tempDict['imagepprint']
-        self.cuisine = tempDict['serves']
+        self.image = tempDict['image']
+        self.cuisine = tempDict['servesCuisine']
         self.rating = tempDict['aggregateRating']['ratingValue']
         self.rate_count = tempDict['aggregateRating']['ratingCount']
-        self.locality = locality
-        self.price_range = price_range
+        self.locality = tempDict['address']['addressLocality']  
+        self.price_range = tempDict['priceRange']
+        self.food_items = []
+
+        data = json.loads(str(res[4])[104:-(2015)])
+        temp_food_items = data['menu']['items']
+        for i in temp_food_items:
+            newFoodItem = Food(temp_food_items[i], self.name)
+            self.food_items.append(newFoodItem)
 
     def __str__(self):
-        return self.name + ", "+ self.locality + ", " + self.city
+        return self.name + ", "+ self.locality + ", " + self.city + ', first food - '+ str(self.food_items[0]) +", size ="+ str(len(self.food_items))
         
 
 
