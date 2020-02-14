8a2c1970c7ad49ebf6a7c0a04c4c443edcd67884
Ace201m <mayank.mayank.cse16@itbhu.ac.in>
2020-02-14 22:12:56 +0530

    made data collection

--- data_collection.py
+++ data_collection.py
@@ -1,5 +1,6 @@
 from src.model.Restaurent import Restaurent
 from src.model.Food import Food
+import pickle
 
 import requests
 
@@ -7,14 +8,18 @@ if __name__ == "__main__":
 
     api_link = r"https://test-es.edamam.com/search?q={}&app_id=3e3dc607&app_key=14976a9356fa2d90fec6beb3ce89855c"
 
-    dishes = set()
+    dishes = []
+    restaurents = []
     with open('rest_link.txt') as f:
         for link in f.readlines():
             print(link)
             rest = Restaurent(link)
+            restaurents.append(rest)
             for food in rest.getFoodList():
-                dishes.add(food.getCategory())
+                dishes.append(food)
 
-    print(len(dishes))
-    print(dishes)
-                
+    with open('food_dataset.pkl', 'wb') as f:
+        pickle.dump(dishes, f)    
+
+    with open('rest_dataset.pkl', 'wb') as f:
+        pickle.dump(restaurents, f)
