6f93b998ece0980030a2f7e3760b4f5a882dbeae
Ace201m <mayank.mayank.cse16@itbhu.ac.in>
2020-02-14 18:25:53 +0530

    added files

--- khush-test.py
+++ khush-test.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests, json
+from pprint import pprint
 
 link = "https://www.swiggy.com/restaurants/al-baik-world-ravindrapuri-lanka-varanasi-78246"
 link2 = "https://www.swiggy.com/restaurants/capsicum-lanka-varanasi-88160"
@@ -9,5 +10,14 @@ res = requests.get(link2)
 soup = BeautifulSoup(res.text, 'html.parser')
 res = soup.find_all('script')
 dic = json.loads(str(res[3])[35:-9])
-print(dic)
-print(str(res[4])[104:-10])
+# print(dic)
+
+# with open('capsicum.txt', 'w') as f:
+#     f.write(str(res[4])[104:-(10 + 171 + 197 + 1637)])
+data = json.loads(str(res[4])[104:-(10 + 171 + 197 + 1637)])
+
+
+print(data['menu'].keys())
+food_items = data['menu']['items']
+
+pprint(food_items)
--- src/model/Food.py
+++ src/model/Food.py
@@ -1,12 +1,12 @@
 class Food:
 
-    def __init__(self, name, price, isveg, category, description, rest_list):
-        self.name = name 
-        self.price = price 
-        self.isveg = isveg
-        self.category = category
-        self.description = description
-        self.rest_list = rest_list
+    def __init__(self, food_dict, rest_name):
+        self.name = food_dict['name']
+        self.price = food_dict['price']
+        self.isveg = food_dict['isVeg']
+        self.category = food_dict['category']
+        self.description = food_dict['description']
+        self.rest = rest_name
 
     def __str__(self):
         return self.name 
